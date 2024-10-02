67a470b1c68b4771214a61aa1e99c10026689789
sany173 <thegogle976@gmail.com>
2024-10-02 14:02:19 +0300

    ww

--- BeginTask-0/ArrayHandler.h
+++ BeginTask-0/ArrayHandler.h
@@ -1,32 +1,53 @@
-
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
 template<typename T>
+
+
 class ArrayHandler {
 private:
-    size_t _size;
-    T* _array;
+    int8_t* _array;
+    int8_t _max;
+    int8_t _min;
+    long long _size;
+    long long _count;
 public:
-    ArrayHandler(size_t size = 10) {
-
+    ArrayHandler(int8_t size = 10){
+        _size = size;
+        _array = new int8_t[_size];
+        _max = INT8_MIN;
+        _min = INT8_MAX;
+        _count = 0;
     }
-
-    void AppendElem(T elem) {
-
+    void AppendElem(int8_t new_element){
+        if (_count == _size) {
+            _size = _size * 4;
+            int8_t* new_arr = new int8_t[_size];
+            std::memcpy(new_arr, _array, _count*sizeof(int8_t));
+            delete [] _array;
+            _array = new_arr;
+        }
+        _array[_count] = new_element;
+        _count++;
+        if (_max < new_element) _max = new_element;
+        if (_min > new_element) _min = new_element;
     }
-
-    bool IsContains(T elem) {
-        return false;
+    int8_t GetMax(){
+        return _max;
     }
-
-    T GetMax() {
-        return 0;
+    int8_t GetMin(){
+        return _min;
     }
+    long long GetIndexElem(int8_t elem){
+        // 1. Доп. память для хранения первого встречного индекса
+        // 2. Бин. поиск
+        for (int8_t i = 0; i < _count; i++) 
+            if (elem == _array[i]) 
+                return i;
 
-    T GetMin() {
-        return 0;
+        return -1;
     }
-
     ~ArrayHandler() {
-
+        delete [] _array;
     }
-
 };
